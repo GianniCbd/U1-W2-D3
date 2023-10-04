8edec26430a1af363b6d8c02dca84c3321833dc9
GianniCbd <giannicabiddu@gmail.com>
2023-10-04 17:02:18 +0200

    U1-W2-D3

--- D3.js
+++ D3.js
@@ -193,7 +193,7 @@ for (let i = 0; i < starWarsCharacters.length; i++) {
   Utilizzando gli elementi presenti nell'array "femaleCharacters" rimuovi dall'array "characters" le stringhe corrispondenti a personaggi con lo stesso nome.
   Una volta fatto crea un console.log per controllare la proprietà length di "characters" prima e dopo l'operazione.
 */
-
+//---
 /* --EXTRA-- ESERCIZIO 10
   Crea una funzionalità che selezioni un elemento casuale dall'array "starWarsCharacters" e ne stampi in console le proprietà in modo discorsivo (a tuo piacimento).
 */
